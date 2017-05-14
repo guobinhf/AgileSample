e57850a8f5bd29e9db969acc0d0ae879c0458817
bingohf <togb@qq.com>
2017-05-14 14:07:44 +0800

    s --> f.length 重命名function name

--- Primes/src/main/java/PrimeGenerator.java
+++ Primes/src/main/java/PrimeGenerator.java
@@ -1,53 +1,51 @@
 public class PrimeGenerator {
-    private static  int s;
     private static boolean[] f;
-    private static int[] primes;
+    private static int[] result;
 
     public static int[] generatePrimes(int maxValue) {
         if (maxValue < 2) {
             return new int[0];
         }else {
-            initializeSieve(maxValue);
-            sieve();
-            loadPrimes();
-            return primes;
+            initializeArrayOfIntegers(maxValue);
+            crossoutMultiples();
+            putUncrossedIntegerIntoResult();
+            return result;
         }
     }
 
-    private static void loadPrimes() {
+    private static void putUncrossedIntegerIntoResult() {
         int i ;
         int j ;
         int count = 0;
-        for (i = 0; i < s; i++) {
+        for (i = 0; i < f.length; i++) {
             if (f[i]) {
                 count++;
             }
         }
-         primes = new int[count];
-        for (i = 0, j = 0; i < s; ++i) {
+         result = new int[count];
+        for (i = 0, j = 0; i < f.length; ++i) {
             if (f[i]) {
-                primes[j++] = i;
+                result[j++] = i;
             }
         }
     }
 
-    private static void sieve() {
+    private static void crossoutMultiples() {
         int i =0;
         int j = 0;
-        for(i =2; i< Math.sqrt(s) + 1;i ++){
+        for(i =2; i< Math.sqrt(f.length) + 1;i ++){
             if (f[i]){
-                for(j = 2 * i; j < s; j +=i){
+                for(j = 2 * i; j < f.length; j +=i){
                     f[j] = false;
                 }
             }
         }
     }
 
-    private static void initializeSieve(int maxValue) {
-        s = maxValue + 1;
-        f = new boolean[s];
+    private static void initializeArrayOfIntegers(int maxValue) {
+        f = new boolean[maxValue +1];
         int i ;
-        for(i =0;i < s; ++i){
+        for(i =0;i < f.length; ++i){
             f[i] = true;
         }
         f[0] = f[1] = false;
