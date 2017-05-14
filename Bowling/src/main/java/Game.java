88e71984fb4427e6c7e0baac42d937bf99dfb05d
bingohf <togb@qq.com>
2017-05-14 17:25:07 +0800

    P46: scoreForFrame implement

--- Bowling/src/main/java/Game.java
+++ Bowling/src/main/java/Game.java
@@ -4,17 +4,23 @@
 public class Game {
 
     private int itsScore;
-
+    private int[] itsThrows = new int[21];
+    private int itsCurrentThrow = 0;
     public int getScore() {
         return itsScore;
     }
 
     //不需要Throw类，Throw没有行为，只是data
     public void add(int pins) {
+        itsThrows[itsCurrentThrow ++ ] = pins;
         itsScore += pins;
     }
 
-    public int getScoreForFrame(int frame) {
-        return 0;
+    public int scoreForFrame(int frame) {
+        int score = 0;
+        for(int ball =0; frame >0 && ball<itsCurrentThrow; ball+=2,frame--){
+            score+= itsThrows[ball] + itsThrows[ball+1];
+        }
+        return score;
     }
 }
--- Bowling/src/test/java/TestGame.java
+++ Bowling/src/test/java/TestGame.java
@@ -10,21 +10,21 @@ public class TestGame {
         assertEquals(5, g.getScore());
     }
 
-    @Test void testTowThrowsNoMark(){
+    @Test public void testTowThrowsNoMark(){
         Game g = new Game();
         g.add(5);
         g.add(4);
         assertEquals(9, g.getScore());
     }
-    @Test void testFourThrowsNoMark(){
+    @Test public void testFourThrowsNoMark(){
         Game g = new Game();
         g.add(5);
         g.add(4);
         g.add(7);
         g.add(2);
         assertEquals(18, g.getScore());
-        assertEquals(9, g.getScoreForFrame(1));
-        assertEquals(18, g.getScoreForFrame(2));
+        assertEquals(9, g.scoreForFrame(1));
+        assertEquals(18, g.scoreForFrame(2));
     }
 
 }
