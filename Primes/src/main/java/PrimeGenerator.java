178429d233a59c787de84e6af7ceef7c0a324b86
bingohf <togb@qq.com>
2017-05-14 14:02:03 +0800

    把一些函数级的局部变量提升为类级别的静态域。我认为这澄清了局部变量和作用范围更大一点的变量的区别

--- Primes/src/main/java/PrimeGenerator.java
+++ Primes/src/main/java/PrimeGenerator.java
@@ -1,34 +1,55 @@
 public class PrimeGenerator {
+    private static  int s;
+    private static boolean[] f;
+    private static int[] primes;
+
     public static int[] generatePrimes(int maxValue) {
-        if (maxValue >= 2) {
-            int s = maxValue + 1;
-            boolean[] f = new boolean[s];
-            int i;
-            for (i = 0; i < s; i++) {
-                f[i] = true;
+        if (maxValue < 2) {
+            return new int[0];
+        }else {
+            initializeSieve(maxValue);
+            sieve();
+            loadPrimes();
+            return primes;
+        }
+    }
+
+    private static void loadPrimes() {
+        int i ;
+        int j ;
+        int count = 0;
+        for (i = 0; i < s; i++) {
+            if (f[i]) {
+                count++;
             }
-            f[0] = f[1] = false;
-            int j;
-            for (i = 2; i < Math.sqrt(s) + 1; i++) {
-                for (j = 2 * i; j < s; j += i) {
+        }
+         primes = new int[count];
+        for (i = 0, j = 0; i < s; ++i) {
+            if (f[i]) {
+                primes[j++] = i;
+            }
+        }
+    }
+
+    private static void sieve() {
+        int i =0;
+        int j = 0;
+        for(i =2; i< Math.sqrt(s) + 1;i ++){
+            if (f[i]){
+                for(j = 2 * i; j < s; j +=i){
                     f[j] = false;
                 }
             }
-            int count = 0;
-            for (i = 0; i < s; i++) {
-                if (f[i]) {
-                    count++;
-                }
-            }
-            int[] primes = new int[count];
-            for (i = 0, j = 0; i < s; ++i) {
-                if (f[i]) {
-                    primes[j++] = i;
-                }
-            }
-            return primes;
-        } else {
-            return new int[0];
         }
+    }
+
+    private static void initializeSieve(int maxValue) {
+        s = maxValue + 1;
+        f = new boolean[s];
+        int i ;
+        for(i =0;i < s; ++i){
+            f[i] = true;
+        }
+        f[0] = f[1] = false;
     }
 }
