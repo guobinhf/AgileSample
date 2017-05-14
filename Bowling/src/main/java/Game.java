f43ddeb4372edd0e6107a41e83d4f96e7260afa9
bingohf <togb@qq.com>
2017-05-14 22:08:32 +0800

    P69：add advanceFrame 将11 独立出来

--- Bowling/src/main/java/Game.java
+++ Bowling/src/main/java/Game.java
@@ -25,19 +25,19 @@ public class Game {
         if(firstThrowInFrame == true){
             if(pins == 10){
                 itsCurrentFrame ++;
+                advanceFrame();
             }else{
                 firstThrowInFrame = false;
             }
         }else{
             firstThrowInFrame = true;
-            itsCurrentFrame++;
+            advanceFrame();
         }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
     }
 
-
-
-
+    private void advanceFrame() {
+        itsCurrentFrame = Math.min(11, itsCurrentFrame +1);
+    }
 
 
     public int getCurrentFrame() {
