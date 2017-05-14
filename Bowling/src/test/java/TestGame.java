d2ca2af1d4d501c2e5bdb6a69c18cbbcbe95e88a
bingohf <togb@qq.com>
2017-05-14 17:34:16 +0800

    P49:  add testSimpleSpare

--- Bowling/src/test/java/TestGame.java
+++ Bowling/src/test/java/TestGame.java
@@ -1,23 +1,30 @@
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 public class TestGame {
-    @Test
+    private Game g;
+
+    @Before
+    public void setUp(){
+        g = new Game();
+    }
+
+   // @Test
     public void testOneThrows(){
-        Game g = new Game();
         g.add(5);
         assertEquals(5, g.getScore());
     }
 
-    @Test public void testTowThrowsNoMark(){
-        Game g = new Game();
+   // @Test
+    public void testTowThrowsNoMark(){
         g.add(5);
         g.add(4);
         assertEquals(9, g.getScore());
     }
-    @Test public void testFourThrowsNoMark(){
-        Game g = new Game();
+  //  @Test
+    public void testFourThrowsNoMark(){
         g.add(5);
         g.add(4);
         g.add(7);
@@ -25,6 +32,12 @@ public class TestGame {
         assertEquals(18, g.getScore());
         assertEquals(9, g.scoreForFrame(1));
         assertEquals(18, g.scoreForFrame(2));
+    }
+    @Test public void testSimpleSpare(){
+        g.add(3);
+        g.add(7);
+        g.add(3);
+        assertEquals(13, g.scoreForFrame(1));
     }
 
 }
