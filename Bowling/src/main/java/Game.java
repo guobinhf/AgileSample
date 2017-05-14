8b30aaa2bfd63efa12c7c4525aa3e9c081199199
bingohf <togb@qq.com>
2017-05-14 18:10:36 +0800

    P55: 一次全中的 implement

--- Bowling/src/main/java/Game.java
+++ Bowling/src/main/java/Game.java
@@ -16,12 +16,16 @@ public class Game {
     public void add(int pins) {
         itsThrows[itsCurrentThrow++] = pins;
         itsScore += pins;
-        adjustCurrentFrame();
+        adjustCurrentFrame(pins);
     }
 
-    private void adjustCurrentFrame() {
+    private void adjustCurrentFrame(int pins) {
         if(firstThrow == true){
-            firstThrow = false;
+            if(pins == 10){
+                itsCurrentFrame ++;
+            }else{
+                firstThrow = false;
+            }
         }else{
             firstThrow = true;
             itsCurrentFrame++;
@@ -33,13 +37,18 @@ public class Game {
         int score = 0;
         for (int currentFrame = 0; currentFrame < thrFrame; currentFrame++) {
             int firstThrow = itsThrows[ball++];
-            int secondThrow = itsThrows[ball++];
-            int frameScore = firstThrow + secondThrow;
-            if (frameScore == 10) {
-                score += frameScore + itsThrows[ball];
-            } else {
-                score += frameScore;
+            if(firstThrow ==10){
+                score += 10 + itsThrows[ball] + itsThrows[ball+1];
+            }else {
+                int secondThrow = itsThrows[ball++];
+                int frameScore = firstThrow + secondThrow;
+                if (frameScore == 10) {
+                    score += frameScore + itsThrows[ball];
+                } else {
+                    score += frameScore;
+                }
             }
+
         }
         return score;
     }
